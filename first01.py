0a71d4e6e87f0339519827b90b23f5026c4d2596
Thitima-164244 <164244@wsk.ac.th>
2026-06-16 04:29:16 +0000

    first01.py

---
+++ first01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวณพื้นที่ของสามหลี่ยม")
+h=float(input("Enter hight:"))
+b=float(input("Enter base:"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
+print("จัดทำโดย นางสาวธิติมา ยอดสุวรรณ์ เลขที่27")
